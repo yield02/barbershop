e6d2f25d9d8319e8d3d33cc0d59de3d601e7f01f
Yield <83912710+yield02@users.noreply.github.com>
2025-10-06 08:24:57 +0700

    feat: add specification for flexible order search

--- barbershop-backend/src/main/java/com/yield/barbershop_backend/specification/OrderSpecification.java
+++ barbershop-backend/src/main/java/com/yield/barbershop_backend/specification/OrderSpecification.java
@@ -1,16 +1,22 @@
 package com.yield.barbershop_backend.specification;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.jpa.domain.Specification;
 
 import com.yield.barbershop_backend.dto.order.OrderFilterDTO;
+import com.yield.barbershop_backend.model.Order;
+import com.yield.barbershop_backend.model.OrderItem;
+import com.yield.barbershop_backend.model.Order.OrderStatus;
 
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.JoinType;
 import jakarta.persistence.criteria.Predicate;
 
 public class OrderSpecification {
-        public static Specification getOrderWithFilter(OrderFilterDTO filter) {
+    public static Specification getOrderWithFilter(OrderFilterDTO filter) {
         return (root, query, criteriaBuilder) -> {
             List<Predicate> predicates = new ArrayList<Predicate>();
 
@@ -34,6 +40,61 @@ public class OrderSpecification {
             }
             
             return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
+        };
+    }
+
+    public static Specification<Order> getOrderDateBetween(Date startDate, Date endDate) {
+        return (root, query, criteriaBuilder) -> {
+            return criteriaBuilder.between(root.get("createdAt"), startDate, endDate);  
+        };
+    }
+
+      public static Specification<Order> getOrderSuccessAndDateBetween(Date startDate, Date endDate) {
+        return (root, query, criteriaBuilder) -> {
+
+            List<Predicate> predicates = new ArrayList<Predicate>();
+
+            predicates.add(criteriaBuilder.greaterThanOrEqualTo(root.get("createdAt"), startDate));
+            predicates.add(criteriaBuilder.lessThanOrEqualTo(root.get("createdAt"), endDate));
+            predicates.add(criteriaBuilder.equal(root.get("status"), OrderStatus.Completed));
+
+            return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
+        };
+    }
+
+    public static Specification<Order> getOrderDrinkAndDateBetween(Date startDate, Date endDate) {
+        return (root, query, criteriaBuilder) -> {
+            query.distinct(true);
+
+            List<Predicate> predicates = new ArrayList<Predicate>();
+
+            predicates.add(criteriaBuilder.greaterThanOrEqualTo(root.get("createdAt"), startDate));
+            predicates.add(criteriaBuilder.lessThanOrEqualTo(root.get("createdAt"), endDate));
+            predicates.add(criteriaBuilder.equal(root.get("status"), OrderStatus.Completed));
+
+            Join<Order, OrderItem> orderItems = root.join("orderItems", JoinType.INNER);
+
+            predicates.add(criteriaBuilder.isNotNull(orderItems.get("drinkId")));
+
+            return criteriaBuilder.and(predicates.toArray(new Predicate[0]));  
+        };
+    }
+
+    public static Specification<Order> getOrderProductAndDateBetween(Date startDate, Date endDate) {
+        return (root, query, criteriaBuilder) -> {
+            query.distinct(true);
+
+            List<Predicate> predicates = new ArrayList<Predicate>();
+
+            predicates.add(criteriaBuilder.greaterThanOrEqualTo(root.get("createdAt"), startDate));
+            predicates.add(criteriaBuilder.lessThanOrEqualTo(root.get("createdAt"), endDate));
+
+
+            Join<Order, OrderItem> orderItems = root.join("orderItems", JoinType.INNER);
+
+            predicates.add(criteriaBuilder.isNotNull(orderItems.get("productId")));
+
+            return criteriaBuilder.and(predicates.toArray(new Predicate[0]));  
         };
     }
 }
